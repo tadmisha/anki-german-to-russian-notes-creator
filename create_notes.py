bbe613735ba32be584ebd28c023ce594df5525fb
tadmisha <tadmisha@gmail.com>
2026-03-19 00:21:01 +0400

    Discard a word if no translation found

--- create_notes.py
+++ create_notes.py
@@ -28,7 +28,8 @@ def main(filename: str):
         completion_time = finishing_time-beginning_time
         print(f"Note created in {completion_time} seconds.\n\n")
 
-        notes.append(note)
+        if note is not None: notes.append(note)
+        else: print(f"No data on the word \"{word}\", discarding.\n\n")
     
     for note in notes:
         save_file(note.audio_filename, note.audio, ANKI_COLLECTIONS_PATH) # pyright: ignore[reportArgumentType]
